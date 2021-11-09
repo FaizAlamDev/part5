04b06b02115c85243c0c2d437c5dcf07fa59de12
Faiz Alam <faizalam9883@protonmail.com>
2021-11-09 17:29:19 +0530

    exercise 5.7
    took 20 mins

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,44 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: 'solid',
+		borderWidth: 1,
+		margin: 5,
+	}
+
+	const [view, setView] = useState(false)
+
+	const handleClick = (e) => {
+		e.preventDefault()
+		setView(!view)
+	}
+
+	if (view === true) {
+		return (
+			<div style={blogStyle}>
+				<div>
+					{blog.title} - {blog.author}
+					<button onClick={handleClick}>hide</button>
+				</div>
+				<div>{blog.url}</div>
+				<div>
+					likes {blog.likes} <button>like</button>
+				</div>
+				<div>{blog.user.name}</div>
+			</div>
+		)
+	}
+
+	return (
+		<div style={blogStyle}>
+			<div>
+				{blog.title} - {blog.author}{' '}
+				<button onClick={handleClick}>view</button>
+			</div>
+		</div>
+	)
+}
 
 export default Blog
