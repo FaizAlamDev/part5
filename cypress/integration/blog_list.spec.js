d6d3d0190e3700a8aa9908a1494a1d71cabaccd4
Faiz Alam <faizalam9883@protonmail.com>
2022-02-19 16:22:58 +0530

    exercise 5.18
    took 20 mins

--- cypress/integration/blog_list.spec.js
+++ cypress/integration/blog_list.spec.js
@@ -1,6 +1,13 @@
 describe('Blog app', function () {
 	beforeEach(function () {
 		cy.request('POST', 'http://localhost:3003/api/testing/reset')
+		// user to backend
+		const user = {
+			name: 'Faiz Alam',
+			username: 'faizalam',
+			password: 'alamalam',
+		}
+		cy.request('POST', 'http://localhost:3003/api/users', user)
 		cy.visit('http://localhost:3000')
 	})
 
@@ -9,5 +16,25 @@ describe('Blog app', function () {
 		cy.contains('username')
 		cy.contains('password')
 		cy.get('#loginBtn').should('contain', 'login')
+	})
+
+	describe('Login', function () {
+		it('succeeds with correct credentials', function () {
+			cy.get('#username').type('faizalam')
+			cy.get('#password').type('alamalam')
+			cy.get('#loginBtn').click()
+
+			cy.contains('faizalam logged in')
+		})
+
+		it('fails with wrong credentials', function () {
+			cy.get('#username').type('faizalam')
+			cy.get('#password').type('alam')
+			cy.get('#loginBtn').click()
+
+			cy.get('.error')
+				.should('contain', 'wrong username or password')
+				.and('have.css', 'color', 'rgb(255, 0, 0)')
+		})
 	})
 })
